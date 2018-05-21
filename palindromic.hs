3958116a2f9107a7b9e4a2b412ea3bc375272e2e
fcracker79 <fcracker79@gmail.com>
2018-05-21 22:01:30 +0200

    Fixed palindromic

--- palindromic.hs
+++ palindromic.hs
@@ -1,5 +1,10 @@
+palindromic' :: String -> Int -> Bool
+palindromic' s i
+    | len == 0 = True
+    | i * 2 >= len = True
+    | otherwise = s !! i == s !! (len - i - 1) && palindromic' s (i + 1)
+    where len = length s
+
 palindromic :: String -> Bool
-palindromic s
-    | s == "" = True
-    | otherwise = last s == s!!0 && palindromic (take 1 (drop 1 s) )
+palindromic s = palindromic' s 0
 
