2337ccd32eb800e5b13ed0661e5217b1adb6dab7
fcracker79 <fcracker79@gmail.com>
2018-06-13 22:33:31 +0200

    Another version of Christmas Tree

---
+++ another_tree.hs
@@ -0,0 +1,25 @@
+module ChristmasTree where
+
+
+data TreeLayer = TreeLayer Int Int
+type Tree = [TreeLayer]
+
+instance Show TreeLayer where
+    show (TreeLayer spaces stars) = concat ((replicate spaces " ") ++ (replicate stars "*"))
+
+
+tree' :: TreeLayer -> [TreeLayer]
+tree' (TreeLayer spaces stars)
+  | stars <= 0 = []
+  | otherwise = (tree' previousLayer) ++ [TreeLayer spaces stars]
+  where previousLayer = TreeLayer (spaces + 1) (stars - 2)
+
+
+tree :: Int -> Either String Tree
+tree h
+  | h <= 0 = Left "Invalid height"
+  | otherwise = Right $ tree' $ TreeLayer noSpaces baseStars where baseStars = 2 * h - 1; noSpaces = 0
+
+printTree :: Either String Tree -> IO ()
+printTree (Right tree) = putStrLn $ concat $ map (\x -> show x ++ "\n") tree
+printTree (Left error) = putStrLn error
