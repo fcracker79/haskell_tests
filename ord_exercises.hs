a80b95b8b3645f8d63d157746acc37a20c1c9b40
fcracker79 <fcracker79@gmail.com>
2018-06-13 22:42:16 +0200

    Ord example

---
+++ ord_exercises.hs
@@ -0,0 +1,13 @@
+module OrdExercises where
+
+data DayOfWeek = Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving (Ord, Show, Eq)
+
+-- instance Eq(DayOfWeek) where
+--     Mon == Mon = True
+--     Tue == Tue = True
+--     Wed == Wed = True
+--     Thu == Thu = True
+--     Fri == Fri = True
+--     Sat == Sat = True
+--     Sun == Sun = True
+--     _ == _ = False
