dfc93c692033bc813825ef59ff750e0e5496ca96
fcracker79 <fcracker79@gmail.com>
2018-07-26 20:24:21 +0200

    Flushed buffered stdout

--- exe/MainChristmasTree.hs
+++ exe/MainChristmasTree.hs
@@ -1,8 +1,10 @@
 module Main where
   import ChristmasTree
+  import System.IO
   main :: IO ()
   main = do
-    putStr "Tree height"
+    putStr "Tree height: "
+    hFlush stdout
     height <- getLine
     let height_int = read height :: Int
     (printTree . tree) height_int
